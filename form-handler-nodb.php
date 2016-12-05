ef52a6381ac317c4b656bed7925506814a12c3ce
yaznet <youcef.ayad.zeddam@gmail.com>
2016-12-05 11:28:38 +0100

    first commit

---
+++ form-handler-nodb.php
@@ -0,0 +1,20 @@
+<?php
+// Emails form data to you and the person submitting the form
+// This version requires no database.
+// Set your email below
+$myemail = "youcef.ayad.zeddam@gmail.com"; // Replace with your email, please
+
+// Receive and sanitize input
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// set up email
+$msg = "Formulaire de contact Le Noroit !\nNom: " . $name . "\nEmail: " . $email . "\nTéléphone: " . $phone . "\nEmail: " . $email;
+$msg = wordwrap($msg,70);
+mail($myemail,"New Form Submission",$msg);
+mail($email,"Merci de votre soumission de formulaire",$msg);
+echo 'Merci pour votre contribution.  S\'il vous plaît <a href="index.html">Cliquez ici pour revenir sur notre page d\'accueil.';
+
+?>
